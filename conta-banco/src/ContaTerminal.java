9a701f08a0543518b9e9a321f8c403686af24ef3
Jhonathan W. <jhonathanwilliam400@gmail.com>
2025-03-09 22:46:23 -0300

    Inserindo nome do cliente

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+
+        Scanner x = new Scanner(System.in);
+        Scanner y = new Scanner(System.in);
+
+        String  entradaTexto  =   "";
+        double  entradaNumero =    0;
+        boolean ok            = true;
+
+        do {
+            System.out.print("Insira seu nome: ");
+            entradaTexto = y.nextLine();
+
+            if(!entradaTexto.matches("[a-zA-ZÀ-ÿ\\s]+")){
+                System.out.println("Entrada invalida, pfv insira somente letras");
+                ok = false;
+            }else{
+                ok = true;
+                conta.setNomeCliente(entradaTexto.toUpperCase());
+            }
+        } while (ok);
+
+        x.close();
+        y.close();
+    }
 }
