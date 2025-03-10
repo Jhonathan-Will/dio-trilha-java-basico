50a309170bdb7687f2e74ca68284b619cbc36990
Jhonathan W. <jhonathanwilliam400@gmail.com>
2025-03-09 21:37:56 -0300

    Iniciando projeto

---
+++ conta-banco/src/Conta.java
@@ -0,0 +1,46 @@
+public class Conta {
+    private int    numeroConta;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public Conta(){
+        this.numeroConta =   0;
+        this.agencia     =  "";
+        this.nomeCliente =  "";
+        this.saldo       = 0.0;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
