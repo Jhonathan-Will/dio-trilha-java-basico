3b2933bafaf660b28fa2f8f419eb06ab73998e7f
Jhonathan W. <jhonathanwilliam400@gmail.com>
2025-03-23 22:01:33 -0300

    Feat: inserindo saldo e mostrando dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -49,12 +49,26 @@ public class ContaTerminal {
             }
         } while (ok);
 
+        do {
+            System.out.print("Insira o saldo da sua conta: ");
+            try {
+                entradaNumero = x.nextDouble();
+                ok = false;
+            } catch (Exception e) {
+                System.out.println("Erro: Insira somente números");
+                ok = true;
+            }
+
+            conta.setSaldo(entradaNumero);
+        } while (ok);
+
+        System.out.println("Olá "+ conta.getNomeCliente() +", Obrigado por cirar uma conta em nosso banco, sua agência é "+ conta.getAgencia() +", conta "+ conta.getNumeroConta() +" e seu saldo "+ conta.getSaldo() +" já está disponivel para saque.");
         x.close();
         y.close();
     }
 
     static String formatarNumeroAgencia(String texto){
-        String parteUm = texto.substring(0, 2);
+        String parteUm = texto.substring(0, 3);
         String parteDois = texto.substring(3);
 
         return parteUm+"-"+parteDois;
